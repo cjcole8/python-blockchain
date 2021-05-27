0d871c622ca90c93f43c3a36a8d0bb036344f7d6
cjcole8 <colecj9@gmail.com>
2021-05-27 16:42:44 -0700

    Initial commit

---
+++ backend/util/crypto_hash.py
@@ -0,0 +1,20 @@
+import hashlib
+import json
+
+
+def crypto_hash(*args):
+    """
+    Return a sha-256 hash of the given args.
+    """
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
+    joined_data = "".join(stringified_args)
+
+    return hashlib.sha256(joined_data.encode("utf-8")).hexdigest()
+
+
+def main():
+    print(f"crypto_hash('foo'): {crypto_hash('sdlkfj')}")
+
+
+if __name__ == "__main__":
+    main()
